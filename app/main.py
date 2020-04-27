ef03b19ee8971de6fa7c8469a5d3cff704f96e1e
Chris Strutton <chris@rodandfly.ca>
2020-04-27 14:25:46 +0000

    get debugging working

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-# import ptvsd
-# ptvsd.enable_attach(address=('0.0.0.0', 5678))
-# ptvsd.wait_for_attach()
+import ptvsd
+ptvsd.enable_attach(address=('0.0.0.0', 5678))
+ptvsd.wait_for_attach()
 
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request
 from pydantic import BaseModel
 
 from .routers import auth
@@ -20,3 +20,9 @@ async def read_users_me(current_user: auth.User = Depends(auth.get_current_activ
 @app.get("/users/me/items/")
 async def read_own_items(current_user: auth.User = Depends(auth.get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
+
+@app.get("/ping")
+async def pong(request: Request):
+    client_host = request.client.host
+    return client_host
+
